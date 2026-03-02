58e48bd930fa33540fd6732abd3aa4dd6848cafb
Albar Malik <albar2806@student.abudzar.sch.id>
2026-03-02 09:27:13 +0700

    gambarr

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 
 export default function Home(){
     return(
-        <section className="px-10.5 py-8 flex flex-col justify-end items-center h-screen bg-[url('/images/learning-together.jpg')] bg-cover">
+        <section className="px-10.5 py-8 flex flex-col justify-end items-center h-screen">
+            <div>
+                <img src="/images/learning-together.jpg" alt="books" className="bg-cover w-full"/>
+            </div>
             <h1 className="font-instrument font-bold text-[150px] -my-8 flex items-center w-full">Minna<span className="inline-block w-full h-3 bg-black"></span>Manabu</h1>
             <h2 className="flex w-full justify-between lowercase"><span className="flex w-[20%]">(everyone)</span>Place where we meet to learn and being. Together.<span className="flex justify-end w-[20%]">(learn)</span></h2>
         </section>    
